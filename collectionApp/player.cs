73ab4fe1ec2496d4b9ed39efe31b0fa7c102e436
SanketKatkar96 <sanketkatkar001@gmail.com>
2026-07-23 15:17:39 +0530

    collectionApp

---
+++ collectionApp/player.cs
@@ -0,0 +1,39 @@
+namespace Worldcup;
+
+//content 
+public class Player
+{
+    
+    private int playerId;
+
+
+    public Player( int playerId,string name,int JerseyNumber, string Position,int age,
+    string nationality,decimal salary)
+    {
+         PlayerId =playerId; 
+         Name = name;
+         jerseyNumber =JerseyNumber;
+         position = Position;
+         Age = age;
+         Nationality = nationality;
+         Salary = salary;
+    }
+
+    //property
+    public int PlayerId
+    {
+        get {return this.playerId ;}
+        set{this.playerId= value;}
+    }
+    
+    //Auto property
+
+    public string Name{get; set;}
+    public int jerseyNumber{get;set;}
+    public string position{get;set;}
+    public int Age {get ;set;}
+    public String Nationality{get;set;}
+    public decimal Salary{get;set;}
+
+
+}
