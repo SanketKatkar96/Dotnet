5611e25ba1977fa9da2ec8eda03d1cc3ba29f860
SanketKatkar96 <sanketkatkar001@gmail.com>
2026-08-25 13:14:03 +0530

    Asyncronization

---
+++ AsynchronousApi/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task<string> GetDataAsync()
+    {
+        await Task.Delay(2000);    //wating for 2 second .
+        return " Data is Loaded";
+
+    }
+    static async Task Main()
+    {
+        try
+        {
+            Console.WriteLine("Hello Sanket...");
+            string data =await GetDataAsync();
+            Console.WriteLine(data);
+
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        
+    }
+
+}
