34e92840e6ff5de0e5e65115772de915c78b88f8
SanketKatkar96 <sanketkatkar001@gmail.com>
2026-08-31 23:36:30 +0530

    handson

---
+++ shadowingMyApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+ 
+
+public class IAnimal
+{
+    public void Sound()
+    {
+        Console.WriteLine("Animal makes a sound");
+    }
+}
+
+ public class Dog :  IAnimal
+{
+    // Shadowing the Sound() method of Animal class
+    public new void Sound()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        IAnimal a = new IAnimal();
+        a.Sound();   // Calls Animal method
+
+        Dog d = new Dog();
+        d.Sound();   // Calls Dog method (shadowed method)
+
+        IAnimal ad = new ();
+        ad.Sound();  // Calls Animal method because of shadowing
+    }
+}
