bbda702b791aad55595a93dc50e1a74146f76f65
SanketKatkar96 <sanketkatkar001@gmail.com>
2026-09-01 17:09:14 +0530

    Unsafe code

---
+++ UnSafeCode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+ class Program
+{
+   static unsafe void Main(string[] args)
+    {
+        int number =10;
+
+        Console.WriteLine("before Modification");
+        Console.WriteLine("Value: " +number);
+        
+        Console.WriteLine("Address: " +(IntPtr)(&number));
+
+        int*ptr =&number;
+        *ptr = 51;
+
+        Console.WriteLine("\nAfter Modification");
+        Console.WriteLine("Value: " + number);
+        Console.WriteLine("Value Via Pointer: "   + *ptr);
+
+        Console.WriteLine("\nLearning Complete");
+    }
+}
